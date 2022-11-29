6a674d359211fabee39870a573f35e4736f5ae73
ethiobek <arkishasoftware@gmail.com>
2022-11-29 00:11:45 -0800

    Is prime number

--- math.js
+++ math.js
@@ -13,6 +13,19 @@ function factorial(n){
     return n;
 }
 
+function isPrime(n){
+    if(n<2){
+        return false;
+    }
+    for(i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
 // Outputs
 console.log(fibonacci(7))
 console.log(factorial(3))
+console.log(isPrime(1))
